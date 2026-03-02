1f516f443b9670cd4a4e012a2c4010b60f709658
Monyvann <m.men6603@gmail.com>
2026-03-02 14:15:44 -0600

    Feature: add dark mode

--- app/hero-v2/page.tsx
+++ app/hero-v2/page.tsx
@@ -1,4 +1,4 @@
-import HeroSectionV2 from "../components/HeroSectionV2";
+import HeroSectionV2 from "../components/HeroSectionV2-dark";
 
 export default function HeroV2() {
   return <HeroSectionV2 />;
